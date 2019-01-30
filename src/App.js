11e4668eeae326dc4df996bb8c33b136b3714b4f
VictorAlagwu <victoralagwu@gmail.com>
2019-01-30 23:59:40 +0100

    Example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import './App.css';
-import ReactSVG from 'react-svg';
-import World from './world.svg';
+
+import Map from './components/Map';
 
 class App extends Component {
   render() {
@@ -19,32 +19,10 @@ class App extends Component {
           <div className="main">
             <Row>
               <Col xs="12">
-                <ReactSVG 
-                  src={World}
-                  evalScripts="always"
-                  fallback={() => <span>Error!</span>}
-                  loading={() => <span>Loading</span>}
-                  onInjected={(error, svg) => {
-                    if (error) {
-                      console.error(error)
-                      return
-                    }
-                    console.log(svg)
-                  }}
-                  renumerateIRIElements={false}
-                  svgClassName="worldSVG"
-                  svgStyle={{  }}
-                  wrapper="span"
-                  className="wrapper-class-name"
-                  onClick={() => {
-                    console.log('wrapper onClick')
-                  }}
-                />
+                <Map />
               </Col>
             </Row>
           </div>
-         
-         
         </Container>
       </div>
     );
