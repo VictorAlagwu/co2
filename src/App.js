1562c203e5490bcb3be9453a1f1b2ce838d8f55f
VictorAlagwu <victoralagwu@gmail.com>
2019-03-09 19:20:12 +0100

    Flags, and Modification

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import {Helmet} from "react-helmet";
-import { Container, Row, Col, Table} from 'reactstrap';
-import 'd3';
+import { Container, Row, Col, Table, Button} from 'reactstrap';
 import 'topojson';
 import Datamap from 'datamaps';
-import update from 'immutability-helper';
 import countries from './data';
 
 
 for (var x = 0; x < countries.length; x++) {
   countries[x]["newBirth"] = 0;
   countries[x]['newDeath'] = 0;
+  countries[x]['garbageProduction'] = 1;
 }
 var windowVisible = true;
+var rand = Math.floor(Math.random() * 5) + 1;
 
+var westernCountries = {
+  'USA': {fillkey: '#e30dfd'},
+  'CAN': {fillkey: '#e30dfd'}, 
+  'ESP': {fillkey: '#e30dfd'}
+};
 document.addEventListener("visibilitychange", function() {
   console.log( document.visibilityState );
   if(document.visibilityState === "hidden"){
@@ -31,10 +36,11 @@ class App extends Component {
       this.state = {
         countries: countries,
         birthCount: 0,
-        deathCount: 0
+        deathCount: 0,
+        showGarbageData: false
       };
-      setInterval(this.birthMap, 1000);
-      setInterval(this.deathMap, 1500);
+
+      setInterval(this.updateMap, 2000);
     }
 
     resize = () => {
@@ -48,12 +54,21 @@ class App extends Component {
       var worldMap = new Datamap ({
         element: document.getElementById("container"),
         scope: 'world',
-        data: countries,
         responsive: true,
+        aspectRatio: 0.5625,
         projection: 'equirectangular',
         fills: {
-          defaultFill: "#333333"
+          'Birth': '#ffc107',
+          'Death': '#dc3545',
+          // 'GBR': '#8c564b',
+          // 'westernCountries': '#8c564b',
+          defaultFill: "#7f7f7f"
         },
+        data: { 
+          // 'GBR': {fillKey: 'GBR'},
+          // 'FRA': {fillKey: 'FRA'},
+          // 'USA': {fillColor: 'westernCountries'}
+          },
         geographyConfig: {
           borderColor: '#DEDEDE',
           highlightBorderWidth: 2,
@@ -81,85 +96,90 @@ class App extends Component {
       this.worldMap = worldMap;
     }
 
-
-    birthMap = () => {
-      if(windowVisible){
+    updateMap = () => {
+      if(windowVisible) {
         var changePopulation = {};
         var countBirth = 0;
+        var countDeath = 0;
+
+       
+
         countries.map((country, i) => {
-         
-            var birthRatePerSec = (country.birthRate * country.population) / 31557600000;
+          var birthRatePerSec = country.birthRate * country.population / 31557600000;
+          var deathRatePerSec = country.deathRate * country.population / 31557600000;
 
-            changePopulation['population'] = country.population;
+          var randomNumber = Math.random() * (1 - 0) + 0;
 
-            if ( (Math.random() * (1 - 0) + 0) < birthRatePerSec ) {
-                changePopulation[country.codes] = '#ffc107';
-                console.log();
-                country.newBirth++;
-                country.population++;
-                countBirth++
-              }
+          changePopulation['population'] = country.population;
 
-          })
-        
+          // First Check if both Death and Birth occur at the same time for a country
+          if ( randomNumber < birthRatePerSec && randomNumber < deathRatePerSec) {
+            // changePopulation[country.codes] = '#61bd4f';
+              console.log('Both Occur same time');
+          } else {
+            if (randomNumber < deathRatePerSec) {
+              changePopulation[country.codes] = '#dc3545';
+              country.newDeath++;
+              country.population--;
+              countDeath++
+            } 
+            if (randomNumber < birthRatePerSec) {
+              // var newRand = Math.floor(Math.random() * 7) + 1;
+                  changePopulation[country.codes] = '#ffc107'
+                  country.newBirth++;
+                  country.population++;
+                  countBirth++
+                  // setTimeout(, newRand * 1000);
+              } 
+            
+          }
+          
+        })
+
         this.setState( (prevState, props) => ({
           countries: prevState.countries,
-          birthCount: prevState.birthCount + countBirth
-        }));
-        
-        this.worldMap.updateChoropleth(changePopulation, {reset: true});
-      }
-    }
-
-    deathMap = () => {
-      if(windowVisible){
-        var changePopulation = {};
-          var countDeath = 0;
-          countries.map((country, i) => {
-          
-            var deathRatePerSec = (country.deathRate * country.population) / 31557600000;
-
-              changePopulation['population'] = country.population;
-
-              if ( (Math.random() * (1 - 0) + 0) < deathRatePerSec ) {
-                  changePopulation[country.codes] = '#dc3545';
-                  console.log();
-                  country.newDeath++;
-                  country.population--;
-                  countDeath++
-                }
-
-            })
-          
-        this.setState( (prevState, props) => ({
-          countries: prevState.countries,
+          birthCount: prevState.birthCount + countBirth,
           deathCount: prevState.deathCount + countDeath
         }));
+        
         this.worldMap.updateChoropleth(changePopulation, {reset: true});
       }
     }
 
-    componentDidMount() {
-      this.drawMap();
-      setInterval(this.testMap, 1000);
+    handleButtonChange = () => {
+      this.setState(prevstate => ({
+        showGarbageData: !prevstate.showGarbageData
+      }));
     }
+    componentDidMount() {
+      // console.log(this.state.rand);
+      this.drawMap();     
+    }
+
     render() {
       var mapList  = (country, i) => {
           return (
               <tr key={i}>
+                  <td><img className="img-fluid" src={"/country-flags/" + country.code.toLowerCase() + ".png"} alt={ country.name + "Flag"} /></td>
                   <th scope="row">{ country.name }</th>
-                  <td>{ country.code }</td>
-                  <td>{ country.population }</td>
-                  <td>{ country.birthRate }</td>
-                  <td>{ country.deathRate }</td>
-                  <td>{ +(country.carbondioxide * ( country.newBirth - country.newDeath)).toFixed(2) }</td>
-                  <td>{ country.newBirth }</td>
-                  <td>{ country.newDeath }</td>
+                  <td>{ '+' + country.newBirth + ' ,  -' + country.newDeath}</td>
+                  <td>
+                      { this.state.showGarbageData === false ? (
+                            +(country.carbondioxide * ( country.newBirth - country.newDeath)).toFixed(2)
+                        ) : (
+                            country.garbageProduction
+                        )
+                     }
+                  </td>
               </tr>
             )
         }
 
-        var listCountries  = this.state.countries.map(mapList)
+        var listCountries  = this.state.countries.sort((a, b) => {
+          var countryA = a.name;
+          var countryB = b.name;
+          return (countryA < countryB) ? -1 : (countryA > countryB) ? 1 : 0;
+        }).map(mapList)
        
      
       return (
@@ -169,7 +189,7 @@ class App extends Component {
             <title>CO2 Emission</title>
             <link rel="canonical" href="http://mysite.com/example" />
           </Helmet>
-          <Container>
+          <Container fluid>
             <div className="header text-center">
               <Row>
                 <Col sm="12">CO2 Map - Birth and Death Rate</Col>
@@ -177,7 +197,7 @@ class App extends Component {
             </div>
             <hr />
             
-            <div className="main">
+            <div className="main container-fluid">
               <Row>
                 <Col xs="12">
                   <div id="container" ref={this.myMap} />
@@ -192,17 +212,28 @@ class App extends Component {
                       <p>Total Death: {this.state.deathCount}</p>
                     </Col>
                   </Row>
+                  <Row>
+                    <Col sm="12">
+                      <Button onClick={this.handleButtonChange}>
+                          {  this.state.showGarbageData === false ? 
+                                          "Show Marginal Garbage Production" : "Show Marginal CO2Emission "
+
+                          }
+                      </Button>
+                      <hr/>
+                    </Col>
+                  </Row>
                   <Table>
                       <thead>
                           <tr>
+                              <th>Flag</th>
                               <th>Country</th>
-                              <th>Country Code</th>
                               <th>Population</th>
-                              <th>Birth Rate </th>
-                              <th>Death Rate </th>
-                              <th>Marginal CO2</th>
-                              <th>Birth Count</th>
-                              <th>Death Count</th>
+                              <th> 
+                                { this.state.showGarbageData === false ? 
+                                                "Marginal CO2" : "Marginal Garbage Prod."
+                                }
+                                </th>
                           </tr>
                       </thead>
                       <tbody>
