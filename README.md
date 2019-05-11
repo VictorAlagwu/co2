9b698ec717360e8c41d8e2b81fb82197a4372747
VictorAlagwu <victoralagwu@gmail.com>
2019-05-11 19:44:55 +0100

    Linear Gradient: Datamap

--- README.md
+++ README.md
@@ -23,9 +23,9 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 * ~Make the water light blue in the map~
 * ~Table should have two different color~
 * ~Make it embedded, in the embedded widget, show the top 5, make it responsive and table smaller and scroller~
-* Check and verify the data in Data.js, and use one year for all data 
-* Fix the tooltip , check the x and y
-* Refactor code
 * ~Gradient / Stripe of red and yellow~
 * ~Population Increase~
 * ~When embedded, remove "Embed Widget button" and "Show All Countries Dropdown"~
+* Check and verify the data in Data.js, and use one year for all data 
+* Fix the tooltip , check the x and y
+* Refactor code
--- src/App.js
+++ src/App.js
@@ -215,7 +215,7 @@ class App extends Component {
 
         this.drawMap();
         let datamapSVG = document.querySelector('.datamap');
-        datamapSVG.insertAdjacentHTML('afterbegin', '<defs><linearGradient id="redYellow" x1="0%" y1="0%" x2="0%" y2="100%" gradientUnits="userSpaceOnUse"><stop offset="11%" style="stop-color:rgba(255,193,7,1);stop-opacity:1" /><stop offset="87%" style="stop-color:rgba(220,53,69,1);stop-opacity:1" /></linearGradient></defs>');
+        datamapSVG.insertAdjacentHTML('afterbegin', '<defs><linearGradient id="redYellow" x1="0%" y1="0%" x2="0%" y2="100%" ><stop offset="11%" style="stop-color:rgba(255,193,7,1);stop-opacity:1" /><stop offset="87%" style="stop-color:rgba(220,53,69,1);stop-opacity:1" /></linearGradient></defs>');
 
 
         window.addEventListener("resize", this.resize());
